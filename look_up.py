fd96fbaa60c649d9010c2e5575fea37327bc3ac8
dimanu-py <diegomtz126@gmail.com>
2024-03-18 18:46:56 +0100

    feat(comparable-protocol): definition of Comparable protocol to introduce a new type hint.
    This protocol defines the minimum set of features for the keys. It's a way to formalize the comparison rules required to keep the keys for the mapping in order.

---
+++ look_up.py
@@ -0,0 +1,25 @@
+from typing import Any, Protocol
+
+
+class Comparable(Protocol):
+    """
+    This protocol defines what methods are required for a class to be considered comparable.
+    """
+
+    def __eq__(self, other: Any) -> bool:
+        ...
+
+    def __ne__(self, other: Any) -> bool:
+        ...
+
+    def __lt__(self, other: Any) -> bool:
+        ...
+
+    def __le__(self, other: Any) -> bool:
+        ...
+
+    def __gt__(self, other: Any) -> bool:
+        ...
+
+    def __ge__(self, other: Any) -> bool:
+        ...
